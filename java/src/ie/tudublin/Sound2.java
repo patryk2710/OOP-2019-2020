bea7fcef12534e50cd5cc90c8c313e29ab3a5589
unknown <patryk2710dd@gmail.com>
2020-03-09 11:10:57 +0000

    lecture

--- java/src/ie/tudublin/Sound2.java
+++ java/src/ie/tudublin/Sound2.java
@@ -33,7 +33,6 @@ public class Sound2 extends PApplet
 		colorMode(HSB);
 	}
 
-<<<<<<< HEAD
 	public int countZeroCrossings() {
 		int count = 0;
 		for (int i = 1; i < as.bufferSize(); i++) 
@@ -45,27 +44,10 @@ public class Sound2 extends PApplet
 		}
 		return count;
 	}
-=======
-	int countZeroCrossings()
-	{
-		int count = 0;
-
-		for(int i = 1 ; i < as.bufferSize() ; i ++)
-		{
-			if (as.left.get(i-1) > 0 && as.left.get(i) <= 0)
-			{
-				count ++;
-			}
-		}
-		return count;
-	}
 
 	float lerpedw = 0;
 	float average = 0;
->>>>>>> 6af089ddab0d2d9e3233de67fdf24864f9f11eff
-
-	float lerpedw = 0;
-	float average = 0;
+	
 	public void keyPressed()
 	{
 		if (key == ' ')
@@ -77,25 +59,7 @@ public class Sound2 extends PApplet
 	}
 
 	float offs = 0;
-	
-	public void circleVisual {
-		float circx = width / 2;
-		float circy = height / 2;
-		int radius = 500;
 
-		for(int i = 0 ; i < as.bufferSize() ; i ++) {
-			float theta = map(i, 0, as.bufferSize(), 0, TWO_PI);
-			float x = circx + sin(theta) * circx * abs(as.left.get(i));
-			float x = circx + sin(theta) * circx * abs(as.left.get(i));
-			stroke(
-				map(i + offs, 0, as.bufferSize()), 0, 255) % 255
-				, 255
-				, 255
-			);
-			line(circx,circy, x, y);
-		}
-		offs += average * 10f;
-	}
 	public void draw()
 	{	
 		background(0);		
@@ -122,13 +86,8 @@ public class Sound2 extends PApplet
 			, 255
 			, 255
 		);
-<<<<<<< HEAD
 		// ellipse(400 , cy,w, w);
 		// ellipse(600 , cy,lerpedw, lerpedw);	
-=======
-		//ellipse(400 , cy,w, w);
-		//ellipse(600 , cy,lerpedw, lerpedw);	
->>>>>>> 6af089ddab0d2d9e3233de67fdf24864f9f11eff
 		
 		int count = countZeroCrossings();
 
@@ -150,10 +109,5 @@ public class Sound2 extends PApplet
 
 		text("FFT Freq: " + freq1, 100, 100);
 
-<<<<<<< HEAD
-		circleVisual();
-		
-=======
->>>>>>> 6af089ddab0d2d9e3233de67fdf24864f9f11eff
 	}
 }
